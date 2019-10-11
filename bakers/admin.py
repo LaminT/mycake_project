e1aa99b84302ab3b249a41d9d2825a8f38d4ad41
LaminT <aka_lamin@hotmail.com>
2019-10-11 16:28:08 +0200

    added pagination, order & filter

--- bakers/admin.py
+++ bakers/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-
 from .models import Baker
 
-admin.site.register(Baker)
+
+class BakerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+
+
+admin.site.register(Baker, BakerAdmin)
--- listings/admin.py
+++ listings/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-
-
 from .models import Listing
 
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_date', 'baker')
+    list_display_links = ('id', 'title')
+    list_filter = ('baker',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address',
+                     'city', 'zipcode', 'price')
+    list_per_page = 25
+
+
+admin.site.register(Listing, ListingAdmin)
