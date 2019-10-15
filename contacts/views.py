fd4d9d85dd3b9b16eb0122b661191e00c6f01a56
LaminT <aka_lamin@hotmail.com>
2019-10-15 20:19:57 +0200

    contact from submission

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Contact
 
 
 def contact(request):
-    return render(request,)
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        baker_email = request.POST['baker_email']
+
+        contact = Contact(listing=listing, listing_id=listing_id, name=name,
+                          email=email, phone=phone, message=message, user_id=user_id)
+
+        contact.save()
+
+        messages.success(
+            request, 'Your request has been successfully submitted, a baker will get back to you shortly.')
+        return redirect('/listings/'+listing_id)
