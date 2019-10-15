28b0cc891de1a9394c6cb78413b8a24cd94946c7
LaminT <aka_lamin@hotmail.com>
2019-10-15 18:18:16 +0200

    contacts app & model

--- mycake/settings.py
+++ mycake/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'bakers.apps.BakersConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
