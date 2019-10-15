2679e0770c3ddcda58acd19e380650b7ad2b72e8
LaminT <aka_lamin@hotmail.com>
2019-10-15 21:50:29 +0200

    inquiry check & send email

--- mycake/settings.py
+++ mycake/settings.py
@@ -141,3 +141,11 @@ MEDIA_URL = '/media/'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+
+# Email Configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'aka.lamin82@gmail.com'
+EMAIL_HOST_PASSWORD = 'nupruzndrgxepttg'
+EMAIL_USE_TLS = True
