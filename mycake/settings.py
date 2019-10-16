19617012924431dff650b18262ed5f3c45fe5901
LaminT <aka_lamin@hotmail.com>
2019-10-17 01:29:32 +0200

    Added local settings import

--- mycake/settings.py
+++ mycake/settings.py
@@ -149,3 +149,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'aka.lamin82@gmail.com'
 EMAIL_HOST_PASSWORD = 'nupruzndrgxepttg'
 EMAIL_USE_TLS = True
+
+try:
+    from.local_settings import *
+except ImportError:
+    pass
